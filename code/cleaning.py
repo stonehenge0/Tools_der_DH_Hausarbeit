57733b885afece235b1f7ea6087d96905ed17255
stonehenge0 <em.stein29@gmail.com>
2024-02-22 13:42:57 +0100

    add cleaned Sherlock text

--- code/cleaning.py
+++ code/cleaning.py
@@ -1,87 +1,78 @@
 """Reading in both Sherlock texts and basic cleaning.
 """
 
+# This script can used both for cleaning both the Sherlock text and for the Enola text.
+# Use the clean_Sherlock() or clean_Enola() functions respectively and adapt the filepaths. 
+
+
 import os
 
 import re
 
 def remove_extra_spaces(input_text):
-    """
-    Remove consecutive spaces in a text.
+    """Remove consecutive spaces in a text."""
+    return re.sub(r'\s+', ' ', input_text)
 
-    This function takes an input text and uses a regular expression
-    to replace consecutive spaces (including tabs and newlines) with a single space.
-
-    Parameters:
-    - input_text (str): The input text with potential consecutive spaces.
-
-    Returns:
-    str: The cleaned text with consecutive spaces replaced by a single space.
-    """
+def replace_quotes_and_lowercase(input_text):
+    """Replace quotes and convert text to lowercase."""
+    # Replace quotes
+    cleaned_text = input_text.replace("“", "\"").replace("”", "\"")
     
-    cleaned_text = re.sub(r'\s+', ' ', input_text)
-    return cleaned_text
+    # Lowercase everything
+    modified_chars = [char.lower() if char.isupper() else char for char in cleaned_text]
+    return "".join(modified_chars)
 
-def clean_Enola (Enola_text):
-    """A cleaning function designed specifially for the traits in the Enola text.
+def clean_Enola(Enola_text):
+    """Clean Enola Holmes text."""
+    # Remove quotes at start of paragraphs and replace stylized ones with Python-readable ones
+    Enola_text = Enola_text.replace("\"", "").replace("“", "\"").replace("”", "\"")
     
-    Gets rid of triple quotes. Replaces stylysed quotes with python-readable ones. Lowercases everything.
-    Remove "\t" and "\n". 
+    # Replace quotes and convert text to lowercase
+    cleaned_enola = replace_quotes_and_lowercase(Enola_text)
+    
+    # Remove "\t" and "\n"
+    cleaned_enola = cleaned_enola.replace('\t', '').replace('\n', ' ')
+    
+    # Remove consecutive spaces
+    cleaned_enola = remove_extra_spaces(cleaned_enola)
+    
+    return cleaned_enola
 
-    Args:
-        Enola_text (str): a utf8 string of Enola Holmes. 
-    Returns:
-        str: cleaned string. 
-    """
+def clean_Sherlock(Sherlock_text):
+    """Clean Sherlock Holmes text."""
+    # Replace quotes and convert text to lowercase
+    cleaned_sherlock = replace_quotes_and_lowercase(Sherlock_text)
     
-    # Remove quotes at start of paragraphs and replace stylised ones with python-readably ones. 
-    Enola_text = Enola_text.replace("\"", "")
-    Enola_text = Enola_text.replace("“", "\"")
-    Enola_text = Enola_text.replace("”", "\"")
+    # Remove "\t" and "\n"
+    cleaned_sherlock = cleaned_sherlock.replace('\t', ' ').replace('\n', ' ')
     
+    # Remove consecutive spaces
+    cleaned_sherlock = remove_extra_spaces(cleaned_sherlock)
     
-    #Lowercase everything. 
-    modified_chars = []
-
-    for char in Enola_text:
-        if char.isupper():
-            modified_chars.append(char.lower())
-        else:
-            modified_chars.append(char)
-            
-    lowercase_enola = "".join(modified_chars)
-            
-    # Remove "\t" and "\n"      
-    lowercase_enola = lowercase_enola.replace('\t', '').replace('\n', ' ')
-    cleaned_enola = remove_extra_spaces(lowercase_enola)
-            
-    return (cleaned_enola)
+    return cleaned_sherlock
 
 
-    
-    
-    
-# Get filepaths. 
-base_dir = os.path.dirname(__file__) 
+# Get filepaths.
+base_dir = os.path.dirname(__file__)
 parent_dir = os.path.dirname(base_dir)
-
-filename = "Full_Enola_utf8.txt"
+filename = "Sherlock_original.txt"
 folder_name = "Textdaten"
 filepath = os.path.join(parent_dir, folder_name, filename)
 
 with open(filepath, 'r', encoding='utf-8') as file:
     file_content = file.read()
-    
-  
+
+
+
+cleaned_sherlock = clean_Sherlock(file_content)
+
+# Writing to output file.  
 if __name__ == "__main__":
-    # Writing to output file. 
-    final_cleaned_file = clean_Enola(file_content)  
-    output_file_path = 'Enola_cleaned.txt'
+    
+    final_cleaned_file = clean_Sherlock(file_content)  
+    output_file_path = 'Sherlock_cleaned.txt'
 
     with open(output_file_path, 'w', encoding='utf-8') as file:
         file.write(final_cleaned_file)
 
     print("Cleaned Text has been written to", output_file_path)
-
-
-
